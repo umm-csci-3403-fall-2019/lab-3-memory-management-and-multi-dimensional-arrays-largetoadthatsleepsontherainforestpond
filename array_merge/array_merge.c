c0f5942f36683be143213a757971301e203a8c51
robe1606 <robe1606@morris.umn.edu>
2019-10-01 12:07:10 -0500

    Attempt nested for loops for array_merge

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -2,5 +2,24 @@
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
   // This is obviously broken. It has the right type, though, eh?
-  return sizes;
+  int* result;
+  int k;
+  int* destination;
+  destination = (int*) calloc(values+1, sizeof(int));
+  for(int i=0; i<sizeof(values); i++){
+	  bool isEqual = true;
+	  for(int j=0; j<sizes[i]; j++){
+		if(destination[i] == null){
+		   destination[j] = values[i][j]
+		}else while(!isEqual || destination != null){
+	           if(values[i][j] != destination[j]){
+                     destination[j] = values[i][j]
+	             isEqual = true;
+		   }
+	        }
+                
+	       	
+	}
+ }
+  return result;
 }
