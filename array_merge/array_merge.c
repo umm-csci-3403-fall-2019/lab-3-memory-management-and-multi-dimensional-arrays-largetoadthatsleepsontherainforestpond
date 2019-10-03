d0b87c71c019d965e55b1f14806677b43abab64f
JoshuaHamann <haman127@morris.umn.edu>
2019-10-02 19:13:21 -0500

    restructures array_merge still does not work properly

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,25 +1,37 @@
 #include "array_merge.h"
-
+#include <stdlib.h>
+#include "../mergesort/mergesort.h"
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
   int* result;
-  int k;
   int* destination;
-  destination = (int*) calloc(values+1, sizeof(int));
-  for(int i=0; i<sizeof(values); i++){
-	  bool isEqual = true;
-	  for(int j=0; j<sizes[i]; j++){
-		if(destination[i] == null){
-		   destination[j] = values[i][j]
-		}else while(!isEqual || destination != null){
-	           if(values[i][j] != destination[j]){
-                     destination[j] = values[i][j]
-	             isEqual = true;
-		   }
-	        }
-                
-	       	
+  int placeholder = 0;//to hold our place for the next interation in the loop
+  int m=0;//sum of all the elements
+  for(int i=0; i<num_arrays; i++){
+      m = sizes[i] + m;
+  }
+  destination = (int*) calloc(m, sizeof(int));
+  for(int i=0; i<num_arrays; i++){
+	for(int j=0; j<sizes[i]; j++){
+            destination[j+placeholder] = values[i][j]; 
 	}
- }
+	placeholder = sizes[i];
+  }
+  mergesort(m,destination);
+ 
+  int k =1;
+  for(int i=1; i<m; i++){
+       if(destination[i] != destination[i-1]){
+               k++;
+       }       
+  }
+  result = (int*) calloc(k+1, sizeof(int));
+  result[0] = k;
+  result[1] = destination[0];
+  for(int i=1;i<m;i++){
+	  if(destination[i] != destination[i-1]){
+	      result[i+1] = destination[i];
+	  }
+  }
+  free(destination); 
   return result;
 }
